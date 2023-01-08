aedbc70ee081a60513f93665b7ae0ce3daaa7215
Konstantin S <97kostada@gmail.com>
2023-01-08 15:27:00 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 2500;
+
+        int miles = 20;
+
+        int bonusMiles = ticketPrice / miles;
+        System.out.println("Количество бонусных миль равно " + bonusMiles);
+    }
+}
